d35fdd3875d3b117ddb01af68ca5d6d4c58f3734
marvin menchu <mmenchu@outlook.com>
2020-06-02 23:02:46 -0600

    agregando animacion para cuenta regresiva con countdown

--- js/main.js
+++ js/main.js
@@ -150,9 +150,20 @@
     // animaciones para los numeros
     $(".resumen-evento").on("mouseenter", function () {
       $(".resumen-evento li:nth-child(1) p").animateNumber({ number: 6 }, 1000);
-      $(".resumen-evento li:nth-child(2) p").animateNumber({ number: 15 }, 1000);
+      $(".resumen-evento li:nth-child(2) p").animateNumber(
+        { number: 15 },
+        1000
+      );
       $(".resumen-evento li:nth-child(3) p").animateNumber({ number: 3 }, 1000);
       $(".resumen-evento li:nth-child(4) p").animateNumber({ number: 9 }, 1000);
+    });
+
+    // Cuenta Regresiva
+    $(".cuenta-regresiva").countdown("2020/09/16", function (event) {
+      $("#dias").html(event.strftime("%D"));
+      $("#horas").html(event.strftime("%H"));
+      $("#minutos").html(event.strftime("%M"));
+      $("#segundos").html(event.strftime("%S"));
     });
   }); // dom content loaded
 })();
