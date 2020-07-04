512f7666e2f5b6a3aa20b4f9c7b0b4a8457c18bf
marvin menchu <mmenchu@outlook.com>
2020-07-04 05:20:12 -0600

    agregnado pagina calendario.php

---
+++ calendario.php
@@ -0,0 +1,12 @@
+<?php
+  include_once 'includes/templates/header.php';
+?>
+    <section class="seccion contenedor">
+      <h2>Calendario de Eventos</h2>
+      
+    </section>
+    <!--section-->
+
+<?php
+  include_once 'includes/templates/footer.php'
+?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -80,7 +80,7 @@
         <!-- menu movil -->
         <nav class="navegacion-principal clearfix">
           <a href="conferencia.php">Conferencia</a>
-          <a href="#">Calendario</a>
+          <a href="calendario.php">Calendario</a>
           <a href="#">Invitados</a>
           <a href="registro.php">Reservaciones</a>
         </nav>
