406d71eda47d20eb473195b3a7401ab891071302
marvin menchu <mmenchu@outlook.com>
2020-07-03 23:40:31 -0600

    creando query para la tabla eventos

--- calendario.php
+++ calendario.php
@@ -3,7 +3,27 @@
 ?>
     <section class="seccion contenedor">
       <h2>Calendario de Eventos</h2>
-      
+      <?php try {
+        require_once('includes/funciones/dbconexion.php');
+        $sqlQuery = "SELECT 
+                  ev.evento_id,
+                  ev.nombre_evento,
+                  ev.fecha_evento,
+                  ev.hora_evento,
+                  ev.clave,
+                  ev.id_cat_evento,
+                  ev.id_invitado
+                  FROM proyectoconferencias.evento as ev;";
+       $datos = $conexion->query($sqlQuery);
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }?>
+
+      <div class="calendario">
+        <?php
+        echo $sqlQuery;
+        ?>
+      </div>
     </section>
     <!--section-->
 
