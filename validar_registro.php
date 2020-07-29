0774ee4c1b7d022b3d826795be99920846fd2e7b
Marvin Menchú <mmenchu@outlook.com>
2020-07-28 23:02:56 -0600

    verificando que los datos de registro sean enviados por un boton de submit

--- validar_registro.php
+++ validar_registro.php
@@ -1,8 +1,10 @@
 <?php include_once 'includes/templates/header.php'?>
 <section class="seccion contenedor">
   <h2>Resumen Registro</h2>
+  <?php if(isset($_POST['submit'])){ ?>
   <pre>
     <?php var_dump($_POST); ?>
   </pre>
+  <?php } ?>
 </section>
 <?php include_once 'includes/templates/footer.php'?>
