ffd8b039f9c5306c7e83bf3ceebed11e2ed12ff0
Marvin Menchú <mmenchu@outlook.com>
2020-07-29 23:51:03 -0600

    creando el json para boletos

--- includes/funciones/funciones.php
+++ includes/funciones/funciones.php
@@ -2,8 +2,12 @@
   function productos_jason(&$boletos,&$casmisas = 0, &$etiquetas =0 ){
     $dias = array( 0 => 'un_dia',1 => 'pase_completo',2=>'pase_2Dias' );
     $total_boletos = array_combine($dias,$boletos);
-    echo "<pre>";
-    echo var_dump($total_boletos);
-    echo "</pre>";
+    $json = array();
+    foreach( $total_boletos as $llave => $boletos) :
+      if((int) $boletos > 0 ):
+        $json[$llave] = (int) $boletos; 
+      endif;
+    endforeach;
+    return json_encode($json);
   }
 ?>
